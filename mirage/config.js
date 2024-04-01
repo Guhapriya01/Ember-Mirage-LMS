422d8a415cd12e49fdbb18d24765c264b5c71ddc
Guhapriya01 <guhapriyadharmaraj08@gmail.com>
2024-04-01 10:27:47 +0530

    Update

--- mirage/config.js
+++ mirage/config.js
@@ -23,6 +23,7 @@ export default function (config) {
 }
 
 function routes() {
+
   this.get('/authors', (schema, request) => {
     // return ['Anonymous', 'John', 'Richard'];
     // let b = schema.authors.find(1)
@@ -30,7 +31,7 @@ function routes() {
     return a;
   });
 
-  // BOOKS
+  // ------------ BOOKS ------------
 
   this.get('/books', (schema, request) => {
     return schema.books.all();
@@ -67,23 +68,4 @@ function routes() {
   );
 
   this.passthrough('https://www.googleapis.com/books/v1/volumes');
-
-  /*
-    Config (with defaults)
-    Note: these only affect routes defined after them!
-  */
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://miragejs.com/docs/getting-started/overview/
-  */
 }
